dabb7a4f74b8a15762d76d1a9968c5d252ca38df
VladimirSivac <morndes@gmail.com>
2024-11-13 22:48:25 +0200

    Task-3 done

--- Program.cs
+++ Program.cs
@@ -8,8 +8,19 @@ namespace ConsoleApp4
 {
     internal class Program
     {
+        // Task 3
         static void Main(string[] args)
         {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+            double b = Func(a);
+            Console.WriteLine("b = {b}");
+            Console.ReadLine();
+        }
+        static double Func(int a)
+        {
+            double b = 12 * Math.Pow(a, 2) + 7 * a - 6;
+            return b;
         }
     }
 }
