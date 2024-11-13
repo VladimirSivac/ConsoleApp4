4cf5e6c22715304296c828dc444b09be2044288e
VladimirSivac <morndes@gmail.com>
2024-11-13 20:26:54 +0200

    Task-2 done

--- Program.cs
+++ Program.cs
@@ -8,8 +8,21 @@ namespace ConsoleApp4
 {
     internal class Program
     {
+        // Task 2
         static void Main(string[] args)
         {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int b = int.Parse(Console.ReadLine());
+            int c = Sum(a, b);
+            Console.WriteLine($"a + b = {c}");
+            Console.ReadLine();
         }
+        static int Sum(int a, int b)
+        { 
+            return a + b; 
+        }
+
     }
 }
