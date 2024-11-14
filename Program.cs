cd884d0bffda746ca1dca4f7e79e75546fd6eb60
VladimirSivac <morndes@gmail.com>
2024-11-14 21:04:55 +0200

    Task-4 done

--- Program.cs
+++ Program.cs
@@ -10,6 +10,31 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            Console.Write("a = ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine($"a + b = {Sum(a, b)}");
+            Console.WriteLine($"a - b = {Razn(a, b)}");
+            Console.WriteLine($"a * b = {Proizv(a, b)}");
+            Console.WriteLine($"a / b = {Del(a, b)}");
+            Console.ReadLine();
+        }
+        static double Sum(double a, double b)
+        { 
+            return a + b; 
+        }
+        static double Razn(double a, double b)
+        {
+            return a - b;
+        }
+        static double Proizv(double a, double b)
+        {
+            return a * b;
+        }
+        static double Del(double a, double b)
+        {
+            return a / b;
         }
     }
 }
